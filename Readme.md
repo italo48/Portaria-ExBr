f6e0277bd432f376bd44e5e6d967422efc787062
Ítalo O Costa <italo.de.oliveira01@gmail.com>
2018-12-17 14:40:08 -0300

    fazendo deploy em tomcat externo

--- Readme.md
+++ Readme.md
@@ -17,3 +17,14 @@
 
 ## Descrição
 A Portaria EB é um pequeno sistema de controle de entrada e saida de pessoal.
+
+## Como rodar o projeto
+ ## Pré-requistos
+    1-MySql instalado
+    2-Tomcat 8.5 rodando
+## Passos
+    1-Baixe o projeto
+    2-Importe como um projeto Maven no Eclipse ou IDE de sua prefência
+    3-Mude o usuario e senha do banco no application.properties
+    4-Navegue até o diretorio do projeto pelo terminal e digite:
+        mvn tomcat7:undeploy deploy
--- src/main/java/br/com/EB/Portaria/PortariaApplication.java
+++ src/main/java/br/com/EB/Portaria/PortariaApplication.java
@@ -2,11 +2,18 @@ package br.com.EB.Portaria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PortariaApplication {
+public class PortariaApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PortariaApplication.class, args);
-	}
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PortariaApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(PortariaApplication.class, args);
+    }
 }
